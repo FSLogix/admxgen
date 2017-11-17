ae17172377958eab74048ec1b7135cd99eb46565
Jeremy Hurren <jhurren@fslogix.com>
2017-11-17 16:51:45 -0700

    Mostly completed parser and generator

--- admxgen/InputParser.cs
+++ admxgen/InputParser.cs
@@ -2,107 +2,262 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace admxgen
 {
-    //class InputParser
-    //{
-    //    private TextReader _reader;
+    class InputParser
+    {
+        private TextReader _reader;
 
-    //    public HashSet<PolicyCategory> Categories = new HashSet<PolicyCategory>();
-    //    public List<Policy> Policies = new List<Policy>();
+        public PolicyDefinitions Definitions { get; } = new PolicyDefinitions
+        {
+            policyNamespaces = new PolicyNamespaces
+            {
+                target = new PolicyNamespaceAssociation()
+            },
+            resources = new LocalizationResourceReference(),
+            supportedOn = new SupportedOnTable
+            {
+                definitions = new SupportedOnDefinition[0]
+            },
+            categories = new CategoryList(),
+            policies = new PolicyList()
+        };
 
-    //    public InputParser(TextReader reader)
-    //    {
-    //        this._reader = reader;
-    //    }
+        public PolicyDefinitionResources Resources { get; } = new PolicyDefinitionResources
+        {
+            resources = new Localization
+            {
+                stringTable = new LocalizationStringTable
+                {
+                    @string = new LocalizedString[0]
+                },
+                presentationTable = new LocalizationPresentationTable()
+                {
+                    presentation = new PolicyPresentation[0]
+                }
+            }
+        };
 
-    //    public void Parse()
-    //    {
-    //        using (var csvReader = new CsvReader(_reader, true))
-    //        {
-    //            var csvHeaders = csvReader.GetFieldHeaders();
-    //            while (csvReader.ReadNextRecord())
-    //            {
-    //                ParseLine(csvReader);
-    //            }
-    //        }
-    //    }
+        public InputParser(TextReader reader)
+        {
+            this._reader = reader;
+        }
 
-    //    private void ParseLine(CsvReader csvReader)
-    //    {
-    //        var category = csvReader["Category"];
-    //        var displayName = csvReader["Display Name"];
-    //        var _class = csvReader["Class"];
-    //        var type = csvReader["Type"];
-    //        var explanation = csvReader["Explanation"];
-    //        var registryKey = csvReader["Registry Key"];
-    //        var valueName = csvReader["Value Name"];
-    //        //var possibleValues = csvReader["Possible Values"];
-    //        //var supportedOn = csvReader["Supported On"];
+        private void AddUniqueArrayItem<T>(Action<T[]> setCollection, T[] collection, T item)
+        {
+            if (collection == null)
+            {
+                collection = new T[0];
+            }
+            var mutableCollection = new List<T>(collection);
+            if (!mutableCollection.Any(i => i.Equals(item)))
+            {
+                mutableCollection.Add(item);
+            }
+            setCollection(mutableCollection.ToArray());
+        }
 
-    //        var categoryObj = new PolicyCategory { Name = category };
-    //        Categories.Add(categoryObj);
-    //        Policies.Add(new Policy {
-    //            Category = categoryObj,
-    //            Class = (PolicyClass) Enum.Parse(typeof(PolicyClass), _class),
-    //            DisplayName = displayName,
-    //            Explanation = explanation,
-    //            RegistryKey = registryKey,
-    //            Type = (PolicyType)Enum.Parse(typeof(PolicyType), type),
-    //            ValueName = valueName
-    //        });
-    //    }
-    //}
+        BooleanElement GetBooleanElement(string policyId, string key, string valueName)
+        {
+            return new BooleanElement { id = policyId, key = key, valueName = valueName, trueValue = new Value { Item = new ValueDecimal { value = 1 } }, falseValue = new Value { Item = new ValueDecimal { value = 0 } } };
+        }
 
-    ////enum PolicyClass { Machine }
+        private string GetRef(string type, string resourceId)
+        {
+            return $"$({type}.{resourceId})";
+        }
 
-    ////enum PolicyType { checkBox }
+        private string GetResourceId(params string[] ss)
+        {
+            var result = string.Empty;
+            Regex rgx = new Regex("[^a-zA-Z0-9]");
+            foreach (var s in ss)
+            {
+                result += rgx.Replace(s, "_") + "_";
+            }
+            return result;
+        }
 
-    ////class PolicyCategory
-    ////{
-    ////    public string Name { get; set; }
+        private string GetStringRef(string resourceId)
+        {
+            return GetRef("string", resourceId);
+        }
 
-    ////    public override int GetHashCode()
-    ////    {
-    ////        return Name.GetHashCode();
-    ////    }
+        TextElement GetTextElement(string policyId, string key, string valueName)
+        {
+            return new TextElement { id = policyId, key = key, valueName = valueName, required = true };
+        }
 
-    ////    public override bool Equals(object obj)
-    ////    {
-    ////        var c = obj as PolicyCategory;
-    ////        if (c == null) return false;
-    ////        return Name.Equals(c.Name);
-    ////    }
-    ////}
+        public void Parse()
+        {
+            using (var csvReader = new CsvReader(_reader, true))
+            {
+                var csvHeaders = csvReader.GetFieldHeaders();
+                while (csvReader.ReadNextRecord())
+                {
+                    ParseLine(csvReader);
+                }
+            }
+        }
 
-    ////class Policy
-    ////{
-    ////    public PolicyCategory Category { get; set; }
-    ////    public PolicyClass Class { get; set; }
-    ////    public string DisplayName { get; set; }
-    ////    public string Explanation { get; set; }
-    ////    public string RegistryKey { get; set; }
-    ////    public PolicyType Type { get; set; }
-    ////    public string ValueName { get; set; }
+        private string ParseCategory(string category)
+        {
+            string lastCategoryId = string.Empty;
+            foreach (var cat in category.Split(new[] { '\\' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                var categoryStringResourceId = GetResourceId("Cat", cat);
+                var categoryId = GetResourceId("Cat", cat);
 
-    ////    public override int GetHashCode()
-    ////    {
-    ////        return HashCodeHelper.CombineHashCodes(Category, Class, DisplayName, Explanation, RegistryKey, Type, ValueName);
-    ////    }
+                AddUniqueArrayItem(c => Resources.resources.stringTable.@string = c, Resources.resources.stringTable.@string, new LocalizedString { id = categoryStringResourceId, Value = cat });
+                var theNewCategory = new Category
+                {
+                    name = categoryId,
+                    displayName = GetStringRef(categoryStringResourceId)
+                };
+                if (!string.IsNullOrEmpty(lastCategoryId))
+                {
+                    theNewCategory.parentCategory = new CategoryReference { @ref = lastCategoryId };
+                }
+                AddUniqueArrayItem(c => Definitions.categories.category = c, Definitions.categories.category, theNewCategory);
 
-    ////    public override bool Equals(object obj)
-    ////    {
-    ////        var p = obj as Policy;
-    ////        if (p == null) return false;
-    ////        return
-    ////            Category.Equals(p.Category) &&
-    ////            Class.Equals(p.Class) &&
-    ////            DisplayName.Equals(p.DisplayName) &&
-    ////            Explanation.Equals(p.Explanation) &&
-    ////            RegistryKey.Equals(p.RegistryKey) &&
-    ////            Type.Equals(p.Type) &&
-    ////            ValueName.Equals(p.ValueName);
-    ////    }
-    ////}
+                lastCategoryId = categoryId;
+            }
+
+            return lastCategoryId;
+        }
+
+        private void ParseLine(CsvReader csvReader)
+        {
+            var category = csvReader["Category"];
+            var displayName = csvReader["Display Name"];
+            var @class = csvReader["Class"];
+            var type = csvReader["Type"];
+            var explanation = csvReader["Explanation"];
+            var registryKey = csvReader["Registry Key"];
+            var valueName = csvReader["Value Name"];
+            var supportedOn = csvReader["Supported On"];
+            var properties = csvReader["Properties"];
+
+            var displayNameStringResourceId = GetResourceId("DisplayName", displayName);
+            AddUniqueArrayItem(c => Resources.resources.stringTable.@string = c, Resources.resources.stringTable.@string, new LocalizedString { id = displayNameStringResourceId, Value = displayName });
+            var displayNameStringRef = GetStringRef(displayNameStringResourceId);
+
+            var explainTextStringResourceId = GetResourceId("Explain", explanation);
+            AddUniqueArrayItem(c => Resources.resources.stringTable.@string = c, Resources.resources.stringTable.@string, new LocalizedString { id = explainTextStringResourceId, Value = explanation });
+            var explainTextStringRef = GetStringRef(explainTextStringResourceId);
+
+            var policyId = GetResourceId("Policy", category, displayName);
+
+            var propertiesDictionary = properties.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries)
+               .Select(part => part.Split('='))
+               .ToDictionary(split => split[0], split => split[1]);
+            
+            // If no label provided, use the policy display name
+            if (!propertiesDictionary.Any(d => d.Key == "Label"))
+            {
+                propertiesDictionary.Add("Label", displayName);
+            }
+
+            var parseTypeResults = ParseType(policyId, type, registryKey, valueName, propertiesDictionary);
+            AddUniqueArrayItem(c => Resources.resources.presentationTable.presentation = c, Resources.resources.presentationTable.presentation, parseTypeResults.Presentation);
+            var presentationStringRef = GetRef("presentation", parseTypeResults.Presentation.id);
+
+            var policy = new PolicyDefinition
+            {
+                name = policyId,
+                parentCategory = new CategoryReference { @ref = ParseCategory(category) },
+                displayName = displayNameStringRef,
+                @class = (PolicyClass)Enum.Parse(typeof(PolicyClass), @class),
+                explainText = explainTextStringRef,
+                key = registryKey,
+                supportedOn = new SupportedOnReference { @ref = ParseSupportedOn(supportedOn) },
+                elements = parseTypeResults.Elements,
+                presentation = presentationStringRef
+            };
+            AddUniqueArrayItem(c => Definitions.policies.policy = c, Definitions.policies.policy, policy);
+        }
+
+        private string ParseSupportedOn(string supportedOn)
+        {
+            var supportedOnStringResourceId = GetResourceId("SupportedOn", supportedOn);
+            var supportedOnId = GetResourceId("SupportedOn", supportedOn);
+
+            AddUniqueArrayItem(c => Resources.resources.stringTable.@string = c, Resources.resources.stringTable.@string, new LocalizedString { id = supportedOnStringResourceId, Value = supportedOn });
+            AddUniqueArrayItem(c => Definitions.supportedOn.definitions = c, Definitions.supportedOn.definitions, new SupportedOnDefinition { name = supportedOnId, displayName = GetStringRef(supportedOnStringResourceId) });
+
+            return supportedOnId;
+        }
+
+        private ParseTypeResult ParseType(string policyId, string type, string key, string valueName, IDictionary<string,string> properties)
+        {
+            var result = new ParseTypeResult();
+            switch (type)
+            {
+                case "checkBox":
+                    result.Elements = new List<object>{ GetBooleanElement(policyId, key, valueName) }.ToArray();
+                    result.Presentation = new PolicyPresentation { id = policyId, Items = new List<object> { new CheckBox { refId = policyId, Value = properties["Label"] } }.ToArray() };
+                    break;
+                case "textBox":
+                    result.Elements = new List<object> { GetTextElement(policyId, key, valueName) }.ToArray();
+                    string defaultValue;
+                    properties.TryGetValue("Default", out defaultValue);
+                    result.Presentation = new PolicyPresentation { id = policyId, Items = new List<object> { new TextBox { refId = policyId, label = properties["Label"], defaultValue = defaultValue } }.ToArray() };
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("valueName", "Unexpected policy type");
+            }
+            return result;
+        }
+
+        private struct ParseTypeResult
+        {
+            public object[] Elements;
+            public PolicyPresentation Presentation;
+        }
+    }
+}
+
+public partial class Category
+{
+    public override bool Equals(object obj)
+    {
+        var category = obj as Category;
+        return this.name.Equals(category.name);
+    }
+
+    public override int GetHashCode()
+    {
+        return name.GetHashCode();
+    }
+}
+
+public partial class LocalizedString
+{
+    public override bool Equals(object obj)
+    {
+        var @string = obj as LocalizedString;
+        return this.id.Equals(@string.id);
+    }
+
+    public override int GetHashCode()
+    {
+        return id.GetHashCode();
+    }
+}
+
+public partial class SupportedOnDefinition
+{
+    public override bool Equals(object obj)
+    {
+        var supportedOnDefinition = obj as SupportedOnDefinition;
+        return this.name.Equals(supportedOnDefinition.name);
+    }
+
+    public override int GetHashCode()
+    {
+        return name.GetHashCode();
+    }
 }
--- admxgen/Program.cs
+++ admxgen/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml;
@@ -9,185 +10,58 @@ namespace admxgen
 {
     class Program
     {
-        //static string Mash(string s)
-        //{
-        //    Regex rgx = new Regex("[^a-zA-Z0-9]");
-        //    return rgx.Replace(s, "_");
-        //}
-
         static void Main(string[] args)
         {
-            var p = new PolicyDefinitions
+            try
             {
-                Revision = "1.0",
-                SchemaVersion = "1.0",
-                PolicyNamespaces = new List<PolicyNamespace>
+                if (args.Length < 2)
                 {
-                    new PolicyNamespace { Prefix = "Fslogix", Namespace = "Fslogix.Policies" }
-                },
-                Resources = new Resources { MinRequiredRevision = "1.0" },
-                SupportedOn = new SupportedOn
-                {
-                    Definitions = new List<Definition>
-                    {
-                        new Definition { Name = "SupportedOn25", DisplayName = "$(string.String_BASE_SupportedOn_FSLogix_Profiles_2_5)" },
-                        new Definition { Name = "SupportedOn26", DisplayName = "$(string.String_BASE_SupportedOn_FSLogix_Profiles_2_6)" }
-                    }
-                },
-                Categories = new List<Category>
-                {
-                    new Category { Name = "Cat_FSLogix", DisplayName = "$(string.String_Cat_FSLogix)" },
-                    new Category { Name = "Cat_PROF_Profile_Container", DisplayName = "$(string.String_PROF_Cat_Profile_Container)", ParentCategory = new Reference { Ref = "Cat_FSLogix" } }
-                },
-                Policies = new List<Policy>
-                {
-                    new Policy
-                    {
-                        Name = "Policy_PROF_Enabled",
-                        Class = "Machine",
-                        DisplayName = "$(string.String_CMN_Policy_Enabled)",
-                        ExplainText = "$(string.String_PROF_Explain_Policy_Enabled)",
-                        Key = "Software\\FSLogix\\Profiles",
-                        Presentation = "$(presentation.Policy_PROF_Enabled)",
-                        ParentCategory = new Reference { Ref = "Cat_PROF_Profile_Container" },
-                        SupportedOn = new Reference { Ref = "SupportedOn25" },
-                        Elements = new Elements
-                        {
-                            List = new List<Element>
-                            {
-                                new BooleanElement
-                                {
-                                    Id = "Policy_PROF_CheckBox_Element_Enabled",
-                                    Key = "Software\\FSLogix\\Profiles",
-                                    ValueName = "Enabled"
-                                }
-                            }
-                        }
-                    },
-                    new Policy
-                    {
-                        Name = "Policy_PROF_VHD_Location",
-                        Class = "Machine",
-                        DisplayName = "$(string.String_Policy_VHD_Location)",
-                        ExplainText = "$(string.String_Explain_Specifies_the_network)",
-                        Key = "Software\\FSLogix\\Profiles",
-                        Presentation = "$(presentation.Policy_VHD_Location)",
-                        ParentCategory = new Reference { Ref = "Cat_PROF_Profile_Container" },
-                        SupportedOn = new Reference { Ref = "SupportedOn25" },
-                        Elements = new Elements
-                        {
-                            List = new List<Element>
-                            {
-                                new TextElement
-                                {
-                                    Id = "Policy_PROF_CheckBox_Element_Enabled",
-                                    Key = "Software\\FSLogix\\Profiles",
-                                    ValueName = "Enabled",
-                                    Required = true
-                                }
-                            }
-                        }
-                    },
-                    new Policy
-                    {
-                        Name = "Policy_PROF_Size_in_MBs",
-                        Class = "Machine",
-                        DisplayName = "$(string.String_Policy_VHD_Location)",
-                        ExplainText = "$(string.String_Explain_Specifies_the_network)",
-                        Key = "Software\\FSLogix\\Profiles",
-                        Presentation = "$(presentation.Policy_VHD_Location)",
-                        ParentCategory = new Reference { Ref = "Cat_PROF_Profile_Container" },
-                        SupportedOn = new Reference { Ref = "SupportedOn25" },
-                        Elements = new Elements
-                        {
-                            List = new List<Element>
-                            {
-                                new DecimalElement
-                                {
-                                    Id = "Policy_PROF_CheckBox_Element_Enabled",
-                                    Key = "Software\\FSLogix\\Profiles",
-                                    ValueName = "Enabled",
-                                    Required = true,
-                                    MaxValue = 10000,
-                                    MinValue = 500
-                                }
-                            }
-                        }
-                    },
-                    new Policy
-                    {
-                        Name = "Policy_PROF_Virtual_Disk_Type",
-                        Class = "Machine",
-                        DisplayName = "$(string.String_Policy_VHD_Location)",
-                        ExplainText = "$(string.String_Explain_Specifies_the_network)",
-                        Key = "Software\\FSLogix\\Profiles",
-                        Presentation = "$(presentation.Policy_VHD_Location)",
-                        ParentCategory = new Reference { Ref = "Cat_PROF_Profile_Container" },
-                        SupportedOn = new Reference { Ref = "SupportedOn25" },
-                        Elements = new Elements
-                        {
-                            List = new List<Element>
-                            {
-                                new EnumElement
-                                {
-                                    Id = "Policy_PROF_CheckBox_Element_Enabled",
-                                    Key = "Software\\FSLogix\\Profiles",
-                                    ValueName = "Enabled",
-                                    Required = true,
-                                    Items = new List<EnumItem>
-                                    {
-                                        new EnumItem { DisplayName = "string", Value = new EnumValue { String = new StringValue { String = "VHD" } } },
-                                        new EnumItem { DisplayName = "string", Value = new EnumValue { String = new StringValue { String = "VHDX" } } }
-                                    }
-                                }
-                            }
-                        }
-                    },
-                    new Policy
-                    {
-                        Name = "Policy_PROF_Virtual_Disk_Type",
-                        Class = "Machine",
-                        DisplayName = "$(string.String_Policy_VHD_Location)",
-                        ExplainText = "$(string.String_Explain_Specifies_the_network)",
-                        Key = "Software\\FSLogix\\Profiles",
-                        Presentation = "$(presentation.Policy_VHD_Location)",
-                        ParentCategory = new Reference { Ref = "Cat_PROF_Profile_Container" },
-                        SupportedOn = new Reference { Ref = "SupportedOn25" },
-                        Elements = new Elements
-                        {
-                            List = new List<Element>
-                            {
-                                new EnumElement
-                                {
-                                    Id = "Policy_PROF_CheckBox_Element_Enabled",
-                                    Key = "Software\\FSLogix\\Profiles",
-                                    ValueName = "Enabled",
-                                    Required = true,
-                                    Items = new List<EnumItem>
-                                    {
-                                        new EnumItem { DisplayName = "string", Value = new EnumValue { Decimal = new DecimalValue { Value = 0 } } }
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    Console.WriteLine("Usage: admxgen [input_file] [output_filename]");
+                    return;
                 }
-            };
 
-            var xmlWriterSettings = new XmlWriterSettings
-            {
-                Indent = true,
-                Encoding = Encoding.UTF8
-            };
+                var parser = new InputParser(new StreamReader(args[0]));
+                parser.Parse();
 
-            using (var xmlWriter = XmlWriter.Create(Console.Out, xmlWriterSettings))
-            {
-                var x = new XmlSerializer(p.GetType(), "http://schemas.microsoft.com/GroupPolicy/2006/07/PolicyDefinitions");
-                x.Serialize(xmlWriter, p);
+                parser.Definitions.revision = "1.0";
+                parser.Definitions.schemaVersion = "1.0";
+                parser.Definitions.policyNamespaces.target.prefix = "FSLogix";
+                parser.Definitions.policyNamespaces.target.@namespace = "FSLogix.Policies";
+                parser.Definitions.resources.minRequiredRevision = "1.0";
+                parser.Definitions.resources.fallbackCulture = "en-US";
+                parser.Definitions.supersededAdm = new List<FileReference>
+                {
+                    new FileReference { fileName = "fslogixODFC2.2.adm" },
+                    new FileReference { fileName = "fslogixODFC2.5.adm" }
+                }.ToArray();
+
+                parser.Resources.revision = "1.0";
+                parser.Resources.schemaVersion = "1.0";
+                parser.Resources.displayName = "FSLogix";
+                parser.Resources.description = "FSLogix Configuration";
+
+                var xmlWriterSettings = new XmlWriterSettings
+                {
+                    Indent = true,
+                    Encoding = Encoding.UTF8
+                };
+
+                using (var w = XmlWriter.Create(new StreamWriter($"{args[1]}.admx"), xmlWriterSettings))
+                {
+                    var ser = new XmlSerializer(parser.Definitions.GetType(), "http://schemas.microsoft.com/GroupPolicy/2006/07/PolicyDefinitions");
+                    ser.Serialize(w, parser.Definitions);
+                }
+
+                using (var w = XmlWriter.Create(new StreamWriter($"{args[1]}.adml"), xmlWriterSettings))
+                {
+                    var ser = new XmlSerializer(parser.Resources.GetType(), "http://www.microsoft.com/GroupPolicy/PolicyDefinitions");
+                    ser.Serialize(w, parser.Resources);
+                }
             }
-
-            Console.WriteLine();
-            Console.ReadKey();
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
