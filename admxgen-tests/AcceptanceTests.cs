49cee0afbc109d297f543e5dcbde7af70f98b4a6
Jeremy Hurren <jhurren@fslogix.com>
2017-12-15 11:19:19 -0700

    Change name of actual output file

--- admxgen-tests/AcceptanceTests.cs
+++ admxgen-tests/AcceptanceTests.cs
@@ -22,21 +22,21 @@ namespace admxgen_tests
             }
         }
 
-        [TestCase("test-data\\checkBoxTest.csv", "test-data\\checkBoxTest-expected")]
-        [TestCase("test-data\\textBoxTest.csv", "test-data\\textBoxTest-expected")]
-        [TestCase("test-data\\enumTest.csv", "test-data\\enumTest-expected")]
-        [TestCase("test-data\\decimalTest.csv", "test-data\\decimalTest-expected")]
-        public void InputFileGeneratesCorrectOutputFile(string input, string expectedOutput)
+        [TestCase("test-data\\checkBoxTest.csv", "test-data\\checkBoxTest")]
+        [TestCase("test-data\\textBoxTest.csv", "test-data\\textBoxTest")]
+        [TestCase("test-data\\enumTest.csv", "test-data\\enumTest")]
+        [TestCase("test-data\\decimalTest.csv", "test-data\\decimalTest")]
+        public void InputFileGeneratesCorrectOutputFile(string input, string outputPrefix)
         {
             var testdir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             Directory.SetCurrentDirectory(testdir);
 
-            if (File.Exists("actual.admx")) File.Delete("actual.admx");
-            if (File.Exists("actual.adml")) File.Delete("actual.adml");
+            if (File.Exists(outputPrefix + "-actual.admx")) File.Delete(outputPrefix + "-actual.admx");
+            if (File.Exists(outputPrefix + "-actual.adml")) File.Delete(outputPrefix + "-actual.adml");
 
             var outputStringBuilder = new StringBuilder();
             var exe = "admxgen.exe";
-            var args = string.Format("{0} actual", input);
+            var args = string.Format("{0} {1}-actual", input, outputPrefix);
             var p = new Process
             {
                 StartInfo = new ProcessStartInfo
@@ -55,15 +55,16 @@ namespace admxgen_tests
             p.WaitForExit();
             Assert.That(p.ExitCode, Is.EqualTo(0), outputStringBuilder.ToString());
 
-            if (CopyIf(expectedOutput + ".admx", "actual.admx") ||
-                CopyIf(expectedOutput + ".adml", "actual.adml"))
+            // Copy the actual generated file to the output file if there is no expected output file (and fail the test)
+            if (CopyIf(outputPrefix + "-expected.admx", outputPrefix + "-actual.admx") ||
+                CopyIf(outputPrefix + "-expected.adml", outputPrefix + "-actual.adml"))
             {
                 Assert.Fail("Expected output files missing");
             }
 
             // Compare
-            AssertFilesAreSame(expectedOutput + ".admx", "actual.admx");
-            AssertFilesAreSame(expectedOutput + ".adml", "actual.adml");
+            AssertFilesAreSame(outputPrefix + "-expected.admx", outputPrefix + "-actual.admx");
+            AssertFilesAreSame(outputPrefix + "-expected.adml", outputPrefix + "-actual.adml");
         }
 
         private static bool CopyIf(string expectedOutputFilename, string actualFilename)
